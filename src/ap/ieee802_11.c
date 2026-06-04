b973e92c48449180f705b590a4d0fa8ec0da1d8c
Ilan Peer <ilan.peer@intel.com>
2026-06-04 09:43:29 +0300

    AP: Refactor the KDK handling in handle_auth_802_1x()
    As a preparation to support PQC AKMs, move the setting
    in handle_auth_802_1x() so that they will not be specific
    only for PMKSA processing.
    
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>
    Change-Id: I53cf669d34dd665b27f03c068c05488b0bddb91b

--- src/ap/ieee802_11.c
+++ src/ap/ieee802_11.c
@@ -3463,6 +3463,11 @@ static void handle_auth_802_1x(struct hostapd_data *hapd, struct sta_info *sta,
 	u16 encap_len, resp = WLAN_STATUS_SUCCESS;
 	const u8 *end;
 	struct wpabuf *reply;
+	bool force_kdk = false;
+
+#ifdef CONFIG_TESTING_OPTIONS
+	force_kdk = hapd->conf->force_kdk_derivation;
+#endif /* CONFIG_TESTING_OPTIONS */
 
 	if (len < 2) {
 		wpa_printf(MSG_INFO, "Missing Encapsulation Length field");
@@ -3559,11 +3564,6 @@ static void handle_auth_802_1x(struct hostapd_data *hapd, struct sta_info *sta,
 			const u8 *aa;
 			enum wpa_alg alg;
 			size_t key_len;
-#ifdef CONFIG_TESTING_OPTIONS
-			bool force_kdk = hapd->conf->force_kdk_derivation;
-#else /* CONFIG_TESTING_OPTIONS */
-			bool force_kdk = false;
-#endif /* CONFIG_TESTING_OPTIONS */
 
 			wpa_hexdump(MSG_DEBUG, "RSNE: STA PMKID",
 				    &data.pmkid[i * PMKID_LEN], PMKID_LEN);
