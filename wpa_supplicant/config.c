4bef007ca54db39355b5fd3b7cc061aa13c5d937
Ilan Peer <ilan.peer@intel.com>
2026-08-05 03:21:40 +0300

    EAP: Validate the configured fragment_size range
    The fragment_size value was used as configured on both the peer and the
    authenticator. The peer assigned it directly to the size_t tls_out_limit
    without the > 0 check that the authenticator side has, so a zero value
    stalled the TLS fragmentation loop on zero length fragments and a
    negative value wrapped around and disabled fragmentation altogether.
    
    Reject values outside 0..65535 when parsing the configuration and fall
    back to the same default as the authenticator when no positive value is
    configured. A new INT_RANGEe() helper is needed since fragment_size is
    stored in the EAP configuration.
    
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>
    Change-Id: I8dcc3175816c4268779806890335cb89a2636718

--- wpa_supplicant/config.c
+++ wpa_supplicant/config.c
@@ -2792,9 +2792,14 @@ static char * wpa_config_write_supported_pqc_constraints(
 #ifdef NO_CONFIG_WRITE
 #define INT_RANGE(f, min, max) #f, wpa_config_parse_int_range, OFFSET(f), \
 	(void *) 0, (void *) (min), (void *) (max), 0
+#define INT_RANGEe(f, m, min, max) #f, wpa_config_parse_int_range, \
+	OFFSET(eap.m), (void *) 0, (void *) (min), (void *) (max), 0
 #else /* NO_CONFIG_WRITE */
 #define INT_RANGE(f, min, max) #f, wpa_config_parse_int_range, \
 	wpa_config_write_int, OFFSET(f),	       \
+	(void *) 0, (void *) (min), (void *) (max), 0
+#define INT_RANGEe(f, m, min, max) #f, wpa_config_parse_int_range, \
+	wpa_config_write_int, OFFSET(eap.m),	       \
 	(void *) 0, (void *) (min), (void *) (max), 0
 #endif /* NO_CONFIG_WRITE */
 
@@ -2961,7 +2966,7 @@ static const struct parse_data ssid_fields[] = {
 #ifdef IEEE8021X_EAPOL
 	{ INT(eap_workaround) },
 	{ STRe(pac_file, pac_file) },
-	{ INTe(fragment_size, fragment_size) },
+	{ INT_RANGEe(fragment_size, fragment_size, 0, 65535) },
 	{ INTe(ocsp, cert.ocsp) },
 	{ INTe(ocsp2, phase2_cert.ocsp) },
 #endif /* IEEE8021X_EAPOL */
