1g4-mirror/hostap

commit 5f1048bf2b355de27a00c47fd97cdcc2ec57003b

PASN: Allow a list of enabled groups to be configured
IEEE Std 802.11-2024, 12.13.1 (Preassociation security negotiation,
General) does not restrict PASN to use only group 19, it mandates group
19 support for interoperability but allows other ECC groups from
dot11RSNAConfigDLCGroupTable. Add a pasn_groups configuration parameter
to allow configuring an ordered list of ECC groups for PASN
authentication. If groups are configured, a group from the configured
list will be used; otherwise, the default group 19 is used.

Signed-off-by: Kavita Kavita <kavita.kavita@oss.qualcomm.com>

wpa_supplicant/config_file.c
@@ -1812,6 +1812,19 @@ static void wpa_config_write_global(FILE *f, struct wpa_config *config)
 		fprintf(f, "pr_pasn_type=%d\n", config->pr_pasn_type);
 	if (config->pr_preferred_role)
 		fprintf(f, "pr_preferred_role=%d\n", config->pr_preferred_role);
+
+#ifdef CONFIG_PASN
+	if (config->pasn_groups) {
+		int i;
+
+		fprintf(f, "pasn_groups=");
+		for (i = 0; config->pasn_groups[i] > 0; i++) {
+			fprintf(f, "%s%d", i > 0 ? " " : "",
+				config->pasn_groups[i]);
+		}
+		fprintf(f, "\n");
+	}
+#endif /* CONFIG_PASN */
 }
 
 static void wpa_config_write_identity(FILE *f, struct wpa_dev_ik *dev_ik)

wpa_supplicant/Android.mk
@@ -427,6 +427,7 @@ NEED_HMAC_SHA256_KDF=y
 NEED_HMAC_SHA384_KDF=y
 NEED_SHA256=y
 NEED_SHA384=y
+NEED_DRAGONFLY=y
 OBJS += src/pasn/pasn_initiator.c
 OBJS += src/pasn/pasn_common.c
 OBJS += pasn_supplicant.c

wpa_supplicant/Makefile
@@ -469,6 +469,7 @@ NEED_HMAC_SHA256_KDF=y
 NEED_HMAC_SHA384_KDF=y
 NEED_SHA256=y
 NEED_SHA384=y
+NEED_DRAGONFLY=y
 OBJS += ../src/pasn/pasn_initiator.o
 OBJS += ../src/pasn/pasn_common.o
 OBJS += pasn_supplicant.o

wpa_supplicant/config.c
@@ -2561,6 +2561,37 @@ static char * wpa_config_write_mac_value(const struct parse_data *data,
 #endif /* NO_CONFIG_WRITE */
 
 
+#ifdef CONFIG_PASN
+static int wpa_config_parse_pasn_groups(const struct parse_data *data,
+					struct wpa_ssid *ssid, int line,
+					const char *value)
+{
+	int *groups;
+
+	groups = wpa_config_parse_int_array(value);
+	if (!groups)
+		return -1;
+	if (groups[0] == 0) {
+		os_free(groups);
+		groups = NULL;
+	}
+	os_free(ssid->pasn_groups);
+	ssid->pasn_groups = groups;
+
+	return 0;
+}
+
+
+#ifndef NO_CONFIG_WRITE
+static char * wpa_config_write_pasn_groups(const struct parse_data *data,
+					 struct wpa_ssid *ssid)
+{
+	return wpa_config_write_freqs(data, ssid->pasn_groups);
+}
+#endif /* NO_CONFIG_WRITE */
+#endif /* CONFIG_PASN */
+
+
 /* Helper macros for network block parser */
 
 #ifdef OFFSET
@@ -2925,6 +2956,9 @@ static const struct parse_data ssid_fields[] = {
 #endif /* CONFIG_IEEE8021X_AUTH */
 	{ BOOL(drop_unicast_ip_in_l2_multicast)},
 	{ INT_RANGE(always_use_proxy_arp, 0, 2)},
+#ifdef CONFIG_PASN
+	{ FUNC(pasn_groups) },
+#endif /* CONFIG_PASN */
 };
 
 #undef OFFSET
@@ -3134,6 +3168,9 @@ void wpa_config_free_ssid(struct wpa_ssid *ssid)
 #ifdef CONFIG_SAE
 	sae_deinit_pt(ssid->pt);
 #endif /* CONFIG_SAE */
+#ifdef CONFIG_PASN
+	os_free(ssid->pasn_groups);
+#endif /* CONFIG_PASN */
 	bin_clear_free(ssid, sizeof(*ssid));
 }
 
@@ -3264,6 +3301,9 @@ void wpa_config_free(struct wpa_config *config)
 	wpabuf_free(config->wps_nfc_dev_pw);
 	os_free(config->ext_password_backend);
 	os_free(config->sae_groups);
+#ifdef CONFIG_PASN
+	os_free(config->pasn_groups);
+#endif /* CONFIG_PASN */
 	wpabuf_free(config->ap_vendor_elements);
 	wpabuf_free(config->ap_assocresp_elements);
 	os_free(config->bgscan);
@@ -5493,6 +5533,37 @@ static int wpa_config_process_sae_groups(
 }
 
 
+#ifdef CONFIG_PASN
+static int wpa_config_process_pasn_groups(
+	const struct global_parse_data *data,
+	struct wpa_config *config, int line, const char *pos)
+{
+	int *groups;
+
+	if (!pos || !*pos) {
+		/* Empty string clears pasn_groups if previously set */
+		if (config->pasn_groups) {
+			os_free(config->pasn_groups);
+			config->pasn_groups = NULL;
+		}
+		return 0;
+	}
+
+	groups = wpa_config_parse_int_array(pos);
+	if (!groups) {
+		wpa_printf(MSG_ERROR, "Line %d: Invalid pasn_groups '%s'",
+			   line, pos);
+		return -1;
+	}
+
+	os_free(config->pasn_groups);
+	config->pasn_groups = groups;
+
+	return 0;
+}
+#endif /* CONFIG_PASN */
+
+
 static int wpa_config_process_ap_vendor_elements(
 	const struct global_parse_data *data,
 	struct wpa_config *config, int line, const char *pos)
@@ -5930,6 +6001,7 @@ static const struct global_parse_data global_fields[] = {
 	{ INT_RANGE(wowlan_disconnect_on_deinit, 0, 1), 0},
 	{ INT_RANGE(rsn_overriding, 0, 2), 0},
 #ifdef CONFIG_PASN
+	{ FUNC(pasn_groups), 0 },
 #ifdef CONFIG_TESTING_OPTIONS
 	{ INT_RANGE(force_kdk_derivation, 0, 1), 0 },
 	{ INT_RANGE(pasn_corrupt_mic, 0, 1), 0 },

wpa_supplicant/config.h
@@ -1828,6 +1828,21 @@ struct wpa_config {
 	enum wpas_rsn_overriding rsn_overriding;
 
 #ifdef CONFIG_PASN
+	/**
+	 * pasn_groups - Preference list of enabled groups for PASN
+	 *
+	 * As per IEEE Std 802.11-2024, 12.13.1, both STAs must have at least
+	 * one cyclic group in common from the dot11RSNAConfigDLCGroupTable for
+	 * ephemeral key exchange. For interoperability, a STA shall support
+	 * group 19 (ECC group defined over a 256-bit prime order field), which
+	 * is used as the default if this parameter is not set. Only suitable
+	 * ECC groups (e.g., 19, 20, 21) are accepted.
+	 *
+	 * If a per-network parameter is configured, it is used. If not, this
+	 * global parameter is used. If neither is set, group 19 is used.
+	 */
+	int *pasn_groups;
+
 #ifdef CONFIG_TESTING_OPTIONS
 	/*
 	 * Normally, KDK should be derived if and only if both sides support

wpa_supplicant/config_ssid.h
@@ -1381,6 +1381,23 @@ struct wpa_ssid {
 	 * 2 = always enable
 	 */
 	int always_use_proxy_arp;
+
+#ifdef CONFIG_PASN
+	/**
+	 * pasn_groups - Preference list of enabled groups for PASN
+	 *
+	 * As per IEEE Std 802.11-2024, 12.13.1, both STAs must have at least
+	 * one cyclic group in common from the dot11RSNAConfigDLCGroupTable for
+	 * ephemeral key exchange. For interoperability, a STA shall support
+	 * group 19 (ECC group defined over a 256-bit prime order field), which
+	 * is used as the default if this parameter is not set. Only suitable
+	 * ECC groups (e.g., 19, 20, 21) are accepted.
+	 *
+	 * If this per-network parameter is not configured, the global
+	 * pasn_groups parameter is used. If neither is set, group 19 is used.
+	 */
+	int *pasn_groups;
+#endif /* CONFIG_PASN */
 };
 
 #endif /* CONFIG_SSID_H */

wpa_supplicant/pasn_supplicant.c
@@ -186,6 +186,29 @@ static int wpas_pasn_sae_setup_pt(struct wpa_ssid *ssid, int group)
 #endif /* CONFIG_SAE */
 
 
+static int wpas_pasn_get_group(struct wpa_supplicant *wpa_s,
+			       struct wpa_ssid *ssid)
+{
+	int i;
+	const int *groups;
+
+	if (ssid && ssid->pasn_groups)
+		groups = ssid->pasn_groups;
+	else if (wpa_s->conf->pasn_groups)
+		groups = wpa_s->conf->pasn_groups;
+	else
+		return 19;
+
+	for (i = 0; groups[i]; i++) {
+		if (dragonfly_suitable_group(groups[i], 1))
+			return groups[i];
+	}
+
+	/* pasn_groups configured but no suitable group found - failure */
+	return 0;
+}
+
+
 static int wpas_pasn_get_params_from_bss(struct wpa_supplicant *wpa_s,
 					 struct pasn_peer *peer,
 					 struct wpa_bss *bss,
@@ -195,7 +218,16 @@ static int wpas_pasn_get_params_from_bss(struct wpa_supplicant *wpa_s,
 	const u8 *rsne, *rsnxe;
 	struct wpa_ie_data rsne_data;
 	int sel, key_mgmt, pairwise_cipher;
-	int group = 19;
+	int group;
+
+	group = wpas_pasn_get_group(wpa_s, ssid);
+	if (!group) {
+		wpa_printf(MSG_INFO,
+			   "PASN: No suitable group found; cannot start authentication");
+		return -1;
+	}
+
+	wpa_printf(MSG_DEBUG, "PASN: Selected group %d", group);
 
 	rsne = wpa_bss_get_rsne(wpa_s, bss, NULL, false);
 	if (!rsne) {

commit 6458cc147bce50e76a09a49d466d2a759fcea3e8

PASN: Write per-network pasn_groups into config file
Only the global pasn_groups parameter was included in configuration file
writing when both the global and per-network pasn_groups parameters were
added.

Fixes: 5f1048bf2b35 ("PASN: Allow a list of enabled groups to be configured")
Signed-off-by: Jouni Malinen <jouni.malinen@oss.qualcomm.com>

wpa_supplicant/config_file.c
@@ -771,6 +771,19 @@ static void write_mka_ckn(FILE *f, struct wpa_ssid *ssid)
 #endif /* CONFIG_MACSEC */
 
 
+#ifdef CONFIG_PASN
+static void write_pasn_groups(FILE *f, struct wpa_ssid *ssid)
+{
+	char *value = wpa_config_get(ssid, "pasn_groups");
+
+	if (!value)
+		return;
+	fprintf(f, "\tpasn_groups=%s\n", value);
+	os_free(value);
+}
+#endif /* CONFIG_PASN */
+
+
 static void wpa_config_write_network(FILE *f, struct wpa_ssid *ssid,
 				     struct wpa_config *config)
 {
@@ -1045,6 +1058,9 @@ static void wpa_config_write_network(FILE *f, struct wpa_ssid *ssid,
 #endif /* CONFIG_IEEE8021X_AUTH */
 	INT_DEF(drop_unicast_ip_in_l2_multicast, 1);
 	INT_DEF(always_use_proxy_arp, 0);
+#ifdef CONFIG_PASN
+	write_pasn_groups(f, ssid);
+#endif /* CONFIG_PASN */
 #undef STR
 #undef INT
 #undef INT_DEF